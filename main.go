3dd7f668571f29b0b6fa5cf3c194fe3f77589f35
Maxime Bouillot <maxbouillot@gmail.com>
2025-09-29 23:18:59 +0200

    Drafted file upload and download.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"net/http"
 
@@ -11,11 +12,45 @@ import (
 func webserver() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+
 	router.GET("/request/:fileId", func(c *gin.Context) {
+		fileId := c.Param("fileId")
+		fmt.Println(fileId)
+
 		c.HTML(http.StatusOK, "file-input.html", gin.H{
-			"fileId": c.Param("fileId"),
+			"FileID": fileId,
 		})
 	})
+	
+	router.GET("/view/:fileId/", func(c *gin.Context) {
+		//fileId := c.Param("fileId")
+		c.File("./uploads/test.jpg")
+	})
+
+	router.POST("/upload/:fileId", func(c *gin.Context) {
+		fileId := c.Param("fileId")
+
+        file, err := c.FormFile("filename")
+        if err != nil {
+            c.String(http.StatusBadRequest, "File upload error: %s", err.Error())
+            return
+        }
+
+		err = os.Mkdir("./uploads/" + fileId, os.ModePerm)
+        if err != nil {
+            c.String(http.StatusBadRequest, "Dir creation error: %s", err.Error())
+            return
+        }
+
+        err = c.SaveUploadedFile(file, "./uploads/" + fileId + "/" + file.Filename)
+        if err != nil {
+            c.String(http.StatusInternalServerError, "Unable to save file: %s", err.Error())
+            return
+        }
+
+        c.String(http.StatusOK, "File %s uploaded successfully.", file.Filename)
+    })
+
 	router.Run("localhost:8080")
 }
 
